9ade5f8f84ec57dd18542f109a0b55ac0eff4732
ryuzaki3248 <gekidannax7@gmail.com>
2023-11-08 15:48:41 +0900

    プッシュします！発展レッスン5-1のカリキュラムです。

--- app/javascript/post.js
+++
@@ -1,9 +0,0 @@
-function post (){
-  const form = document.getElementById("form");
-  form.addEventListener("submit", (e) => {
-    console.log(document.getElementById("form"));
-    e.preventDefault();
-  });
-};
-
-window.addEventListener('turbo:load', post);
